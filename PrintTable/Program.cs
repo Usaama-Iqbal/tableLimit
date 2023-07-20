ab2b0c0aa51fd66a4b8002fe4a63b5fcfa228cd8
Usama Iqbal <uiqbal@cedrus.com>
2023-07-20 16:29:11 +0500

    feat: The user give a number and give a limit number till the table ends

---
+++ PrintTable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class table
+{
+    public static void Main(string[] args)
+    {
+        int result;
+        
+        Console.Write("Please Give a number : ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter the limit : ");
+        int limit = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= limit; i++)
+        {
+             result = number * i;
+            Console.WriteLine(number+ " * " +i+  " = " +result );
+           
+        }
+
+    }
+}
+    
